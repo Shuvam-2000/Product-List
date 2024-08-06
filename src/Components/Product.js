44df607d03f97226eb0ae8d137e5b6b2de5414b6
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-08-06 17:25:01 +0530

    all components present here

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -23,8 +23,8 @@ const Product = () => {
                               return(
                                 item.price >= 100
                               )              
-                })
-                setProductItem(showItem)
+              })
+              setProductItem(showItem)
       }
 
   // filter product by rating
@@ -33,8 +33,8 @@ const Product = () => {
                             return(
                               item.rating >= 4
                             )
-    })
-    setProductItem(filterItem)
+              })
+              setProductItem(filterItem)
   }
 
   // show all products
